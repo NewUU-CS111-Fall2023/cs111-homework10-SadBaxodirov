c758006f5aae83e046774b0fc15e8c26e60d3810
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-20 00:04:49 +0500

    Update task_9.cpp

--- src/cpp/task_9.cpp
+++ src/cpp/task_9.cpp
@@ -3,7 +3,32 @@
  * Date:
  * Name:
  */
+struct Literal {
+    char variable;
+    bool isNegated; 
 
+    Literal(char var, bool neg) : variable(var), isNegated(neg) {}
+};
 class Problem9 {
 public:
+    bool ClauseSatisfied(const unordered_map<char, bool>& truthAssignment, const vector<Literal>& clause) {
+        for (const Literal& literal : clause) {
+            bool value = truthAssignment.at(literal.variable);
+            if (literal.isNegated) {
+                value = !value;
+            }
+            if (value) {
+                return true;
+            }
+        }
+        return false; 
+    }
+    bool CNFSatisfied(const vector<vector<Literal>>& cnfFormula, const unordered_map<char, bool>& truthAssignment) {
+        for (const vector<Literal>& clause : cnfFormula) {
+            if (!ClauseSatisfied(truthAssignment, clause)) {
+                return false;
+            }
+        }
+        return true;  
+    }
 };
