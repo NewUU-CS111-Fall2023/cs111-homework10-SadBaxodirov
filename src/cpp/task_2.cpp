81f39570f07b85c4375c874df5711e115e42cb26
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-19 23:55:52 +0500

    Update task_2.cpp

--- src/cpp/task_2.cpp
+++ src/cpp/task_2.cpp
@@ -3,7 +3,25 @@
  * Date:
  * Name:
  */
-
+/*
+ Q0,# -> Q1,same, Left
+ Q0,0 -> Q0,1, Right
+ Q0,1 -> Q0,same, Right
+ Q1,# -> Q2,same, Right
+ Q1,1 -> Q1,same, Left
+ Q2,1 -> Q3,#, Right
+ Q3,1 -> Halt,#, Stay
+*/
 class Problem2 {
 public:
+    vector<int> add(vector<int> numbers){
+        vector<int> result;
+        for(int i:numbers){
+            if(i!=0){
+                result.push_back(i);
+            }
+        }
+        result.pop_back();
+        return result;
+    }
 };
