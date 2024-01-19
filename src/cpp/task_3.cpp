2d66340a98e70101b11861d2aa273df0e87f4deb
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-19 23:56:05 +0500

    Update task_3.cpp

--- src/cpp/task_3.cpp
+++ src/cpp/task_3.cpp
@@ -5,4 +5,26 @@
  */
 
 class Problem3 {
+public:
+    int problem3(string n1, string n2){
+        int result = 0;
+        if (n1.size()>n2.size())
+            result= 1;
+        else if(n1.size()<n2.size())
+            result= 11;
+        else {
+            for (int i=0;i<n1.size();i++) {
+                if(n1[i]==n2[i]) {
+                    continue;
+                }
+                else if (n1[i]-'0'>n2[i]-'0'){
+                    result= 1;
+                }
+                else {
+                    result= 11;
+                }
+            }
+        }
+        return result;
+    }
 };
