70d5f692ad393afd292865a1b808eb2e0030df50
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-19 23:56:18 +0500

    Update task_4.cpp

--- src/cpp/task_4.cpp
+++ src/cpp/task_4.cpp
@@ -5,4 +5,13 @@
  */
 
 class Problem4 {
+public:
+    vector<int> multiply( vector<int> num1, vector<int> num2) {
+        vector<int> result;
+        for (int i=0;i<num2.size()-1;i++){
+            result.insert(result.end(), num1.begin(),num1.end()-1);
+        }
+        result.push_back(1);
+        return result;
+    }
 };
