0b64f039b450b2218fc83e676ce7af2a6f2801df
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2024-01-19 23:55:35 +0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -6,4 +6,19 @@
 
 class Problem1 {
 public:
+    void problem1(int numbers[], int n) {
+        int sum = 0;
+        vector<int> result;
+        for (int i = 0; i < n; i++) {
+            if (numbers[i] != 0) {
+                sum += numbers[i];
+            }
+            if (numbers[i] == 0) {
+                result.push_back(sum);
+                sum = 0;
+            }
+        }
+        for (int i = 1; i < result.size(); i++)
+            cout << result[i] << " ";
+    }
 };
